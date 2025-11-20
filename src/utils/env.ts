74aee34eb193db8c67ae9b38cd64a622c1bb2e52
Chloe A <chloe@sapphic.moe>
2025-11-20 14:17:53 +0500

    ci: test 4

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,52 +5,63 @@ export const schema = {
   PUBLIC_TURNSTILE_SITE_KEY: envField.string({
     context: 'client',
     access: 'public',
+    default: '',
   }),
   TURNSTILE_SECRET_TOKEN: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
 
   // API Keys
   OWM_API_KEY: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
   LASTFM_API_KEY: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
 
   // Database
   ASTRO_DB_REMOTE_URL: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
   ASTRO_DB_APP_TOKEN: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
   ASTRO_DB_GUESTBOOK_DASHBOARD_URL: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
 
   // Discord
   DISCORD_CONTACT_WEBHOOK_URL: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
   DISCORD_GUESTBOOK_WEBHOOK_URL: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
   DISCORD_USER_ID: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
   LANYARD_API_URL: envField.string({
     context: 'server',
     access: 'secret',
+    default: '',
   }),
 
   // Git commit info
