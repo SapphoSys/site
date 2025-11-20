7b02c3a3330e8fbac14732beaf0407a69e914b95
Chloe A <chloe@sapphic.moe>
2025-11-20 14:44:25 +0500

    ci: revert

--- flake.nix
+++ flake.nix
@@ -40,28 +40,25 @@
       packages = forSystems (
         pkgs:
         let
-          # Pure environment variables with defaults
-          # These will be used for deterministic builds
+          # Build-time environment variables
           buildEnv = {
-            PUBLIC_TURNSTILE_SITE_KEY = "";
-            TURNSTILE_SECRET_TOKEN = "";
-            OWM_API_KEY = "";
-            LASTFM_API_KEY = "";
-            ASTRO_DB_REMOTE_URL = "";
-            ASTRO_DB_APP_TOKEN = "";
-            ASTRO_DB_GUESTBOOK_DASHBOARD_URL = "";
-            DISCORD_CONTACT_WEBHOOK_URL = "";
-            DISCORD_GUESTBOOK_WEBHOOK_URL = "";
-            DISCORD_USER_ID = "";
-            LANYARD_API_URL = "";
-            COMMIT_HASH = "unknown";
-            COMMIT_DATE = "unknown";
+            PUBLIC_TURNSTILE_SITE_KEY = builtins.getEnv "PUBLIC_TURNSTILE_SITE_KEY";
+            TURNSTILE_SECRET_TOKEN = builtins.getEnv "TURNSTILE_SECRET_TOKEN";
+            OWM_API_KEY = builtins.getEnv "OWM_API_KEY";
+            LASTFM_API_KEY = builtins.getEnv "LASTFM_API_KEY";
+            ASTRO_DB_REMOTE_URL = builtins.getEnv "ASTRO_DB_REMOTE_URL";
+            ASTRO_DB_APP_TOKEN = builtins.getEnv "ASTRO_DB_APP_TOKEN";
+            ASTRO_DB_GUESTBOOK_DASHBOARD_URL = builtins.getEnv "ASTRO_DB_GUESTBOOK_DASHBOARD_URL";
+            DISCORD_CONTACT_WEBHOOK_URL = builtins.getEnv "DISCORD_CONTACT_WEBHOOK_URL";
+            DISCORD_GUESTBOOK_WEBHOOK_URL = builtins.getEnv "DISCORD_GUESTBOOK_WEBHOOK_URL";
+            DISCORD_USER_ID = builtins.getEnv "DISCORD_USER_ID";
+            LANYARD_API_URL = builtins.getEnv "LANYARD_API_URL";
           };
           site = pkgs.callPackage ./site.nix { inherit buildEnv; };
           container = pkgs.callPackage ./container.nix {
             inherit pkgs site;
-            commitHash = buildEnv.COMMIT_HASH;
-            commitDate = buildEnv.COMMIT_DATE;
+            commitHash = builtins.getEnv "COMMIT_HASH";
+            commitDate = builtins.getEnv "COMMIT_DATE";
           };
         in
         {
